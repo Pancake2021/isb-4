78d41eab5afee13cd49fee3bfa2e245bd1068dc7
Pancake2021 <92122200+Pancake2021@users.noreply.github.com>
2023-05-29 15:00:32 +0400

    fix for

--- settings.py
+++ settings.py
@@ -5,8 +5,8 @@ logger = logging.getLogger()
 logger.setLevel('INFO')
 
 SETTINGS = {
-    'hash': 'files/hash.txt',
-    'bin': 'files/bin_numbers.txt',
+    'hash': "4f9cbbc1e5a60f526a58cde1343acd900b3f918f6fdff73eb82b0b87ae7d8e22",
+    'bin':  ["427409","427695","424542","424543","424544","442197","442194","442195","442196","479578","480110","480111","480112","480113","480114","481776","481777","481779","483983","427400","427402","427403","427404","427406","427407","427408","427411","427412","427413"],
     'last_four_numbers': 'files/last_four_numbers.txt',
     'card_number': 'files/card_number.txt',
     'csv_statistics': 'files/statistics.csv',
